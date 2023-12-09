4efdec31ad950ecf2e25f2dbf7109c00cff71728
hopp24 <132327055+grace1536hopper@users.noreply.github.com>
2023-12-08 19:54:16 -0600

    Update auth.guard.ts

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -4,24 +4,6 @@ import { DatosAcademicosService } from '../services/datos-academicos.service';
 import { tap } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-// export const authGuard: CanActivateFn = (route, state) => {
-//   const userService = inject(DatosAcademicosService);
-//   const router = inject(Router);
-
-//   console.log('pasaste por el canActivate');
-
-//   return this.userService.validarToken().pipe(
-//     tap((estaAutenticado: any ) => {
-//       if(! estaAutenticado){
-//         router.navigateByUrl('/login');
-//         // verifica que el usuario este autenticado
-//       }
-//     })
-//   )
-
-// };
-
-
 export class AuthGuard implements CanActivate {
   constructor(
     private userService: DatosAcademicosService,
@@ -42,27 +24,3 @@ export class AuthGuard implements CanActivate {
   }
 }
 
-
-
- // .pipe(
-  //   tap((estaAutenticado: any ) => {
-  //     if(! estaAutenticado){
-  //       router.navigateByUrl('/login');
-  //       // verifica que el usuario este autenticado
-  //     }
-  //   })
-  // )
-
-
-  // pipe(
-  //   map((resp) => {
-  //     console.log(resp);
-  //     // Devuelve true si el token es válido, de lo contrario, redirige a la página de login
-  //     if (resp) {
-  //       return true;
-  //     } else {
-  //       router.navigate(['/login']);
-  //       return false;
-  //     }
-  //   })
-  // );
